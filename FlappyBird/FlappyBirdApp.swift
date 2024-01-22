5e9bb83d748253a6f54d1b917a78a25088bf66d5
tatanadubova <tatyana.dubova.1986@gmail.com>
2024-01-22 20:10:27 +0300

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Татьяна Дубова on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
